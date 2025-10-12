a657d45e45f76e5eda3835650d6ee252cb1c848a
davide <davide@WORKSTATION10>
2025-10-12 14:35:12 +0200

    code cleaning

--- BlazorAppInter/Program.cs
+++ BlazorAppInter/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5140") });
+
 
 var app = builder.Build();
 
