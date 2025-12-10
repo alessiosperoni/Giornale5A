dc227df6da218a44fda0ed1ef21c5823a459e8ec
alessiosperoni <alessio.speroni93@gmail.com>
2025-12-10 17:52:00 +0100

    Prova

--- SharedClassLibrary/Messaggio.cs
+++ SharedClassLibrary/Messaggio.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace SharedClassLibrary
+namespace SharedClassLibrary //Prova modifica
 {
     public class Messaggio{
         [Required(ErrorMessage = "Questo campo è obbligatorio")]
