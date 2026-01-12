b50d1fcba1b9605eef4ac3f8f65146267c7a54ab
federico.formicola.s <federico.formicola.s@SIRIO_POST03>
2026-01-12 10:55:49 +0100

    Aggiunti Articolo e ArticoliRepository

---
+++ WebApplicationApi/Controllers/Articolo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClassLibrary
+{     
+    public class Articolo
+    {
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        public int IdArticolo { get; set; }
+
+
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "La lunghezza può essere al massimo di 50 caratteri")]
+        public string titolo { get; set; }
+
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "La lunghezza può essere al massimo di 50 caratteri")]
+        public string autore { get; set; }
+
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        [StringLength(500, ErrorMessage = "La lunghezza può essere al massimo di 500 caratteri")]
+        public string testo { get; set; }
+
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        public DateTime data_Pubblicazione { get; set; }
+    }
+}
---
+++ WebApplicationApi/Data/ArticoliRepositoryJson.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using SharedClassLibrary;
+
+namespace WebApplicationApi.Data
+{
+    public class ArticoliRepositoryJson
+    {
+        private string dataSourceString = @"Data/Source/articoli.json";
+        public Articolo CreateArticoli(string titolo,string autore, DateTime Data_pubblicazione)
+        {
+            List<Articolo> myList = GetArticoliList();
+            Articolo ultimo = myList.Last<Articolo>();
+            Articolo newArticolo = new Articolo();
+            newArticolo.titolo = titolo;
+            newArticolo.IdArticolo = ultimo.IdArticolo + 1;
+            myList.Add(newArticolo);
+
+            JsonSerializer mySerializer = new JsonSerializer();
+            StreamWriter myStream = new StreamWriter(dataSourceString);
+            mySerializer.Serialize(myStream, myList);
+            myStream.Close();
+
+            return newArtist;
+
+        }
+
+        public Artist GetArtist(int Id)
+        {
+            List<Artist> myList = GetArtistsList();
+            return myList.Find(x => x.IdArtist == Id);
+
+        }
+
+        public List<Artist> GetArtistsList()
+        {
+            JsonSerializer mySerializer = new JsonSerializer();
+            StreamReader myStreamreader = File.OpenText(dataSourceString);
+            List<Artist> myList = (List<Artist>)mySerializer.Deserialize(myStreamreader, typeof(List<Artist>));
+            myStreamreader.Close();
+            return myList;
+        }
+    }
+}
